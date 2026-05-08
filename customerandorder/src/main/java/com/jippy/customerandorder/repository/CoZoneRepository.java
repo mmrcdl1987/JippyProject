ebf1df97ef2a770ece9312f6d7037f395149fd58
RohanVadluri8463 <r.vadluri@ajacs.in>
2026-05-08 17:29:07 +0530

    written Api for Co-zoneAssignmentToDriver we are passing params as a driverId,(lattitude,longitude)OR zoneId either of one, by query if longitude or lattitude exists (OR) zoneid exists either of one, then the driverid from Codriver table and zoneid from Cozones table will be stores in the CodriverzoneAssignment table and for one driver we may assign many zones (OR) longitudes and latitudes vice versa.

--- customerandorder/src/main/java/com/jippy/customerandorder/repository/CoZoneRepository.java
+++ customerandorder/src/main/java/com/jippy/customerandorder/repository/CoZoneRepository.java
@@ -12,9 +12,24 @@ import java.util.Optional;
 @Repository
 public interface CoZoneRepository extends JpaRepository<CoZone, Integer> {
 
-    @Query(value = "SELECT EXISTS(SELECT 1 FROM jippy_customer_and_order.zones WHERE ST_Equals(CAST(boundary AS geometry), ST_SetSRID(CAST(:polygon AS geometry), 4326)))",
-    nativeQuery = true)
+    @Query(value = "SELECT EXISTS(SELECT 1 FROM jippy_customer_and_order.zones WHERE ST_Equals(CAST(boundary AS geometry), ST_SetSRID(CAST(:polygon AS geometry), 4326)))", nativeQuery = true)
     boolean existsBySpatialBoundary(@Param("polygon") Polygon polygon);
 
     Optional<CoZone> findByZoneName(String zoneName);
+
+    // Find zone using latitude and longitude and
+    // return the zone which contains the point defined by latitude and longitude
+    @Query(value = """
+            SELECT *
+            FROM jippy_customer_and_order.zones
+            WHERE ST_Intersects(
+                boundary,
+                ST_SetSRID(
+                    ST_MakePoint(:longitude, :latitude),
+                    4326
+                )::geography
+            )
+            LIMIT 1
+            """, nativeQuery = true)
+    CoZone findZoneByCoordinates(@Param("latitude") Double latitude, @Param("longitude") Double longitude);
 }
