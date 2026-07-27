618f10591c42641bfcdb01b5b06f717bd59fa030
Tejaswini <penjurutejaswini3@gmail.com>
2026-07-27 12:42:17 +0530

    Fixed Timezone issue in notification microservice

--- notification/src/main/java/com/jippy/notification/NotificationApplication.java
+++ notification/src/main/java/com/jippy/notification/NotificationApplication.java
@@ -1,14 +1,21 @@
 package com.jippy.notification;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 
 @SpringBootApplication
 public class NotificationApplication {
 
+
 	public static void main(String[] args) {
-		SpringApplication.run(NotificationApplication.class, args);
+
+        // MUST BE BEFORE SpringApplication.run()
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+        SpringApplication.run(NotificationApplication.class, args);
 	}
 
 }
